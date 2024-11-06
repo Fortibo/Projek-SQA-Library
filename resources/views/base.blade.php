f99b6d0a6dde440a7b65423d6751c2e4605a715c
Dohazo <pootisspy931@gmail.com>
2024-11-06 20:25:40 +0700

    dwd

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     @vite('resources/css/app.css')
     <title> wewewe</title>
 </head>
 <body>
     <div>
-        
+        @include('nav')
     </div>
     <div>
         @yield('konten')
