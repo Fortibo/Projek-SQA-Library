eebe12d4ac8624324d341091908a26ace1a18c0b
Dohazo <pootisspy931@gmail.com>
2024-11-06 20:10:56 +0700

    initial commit

---
+++ resources/views/base.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title> wewewe</title>
+</head>
+<body>
+    <div>
+        
+    </div>
+    <div>
+        @yield('konten')
+    </div>
+</body>
+</html>
